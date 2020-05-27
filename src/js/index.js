4a815575b475a105781f53677890a24c2eca338d
Vlad <vladprodan6@gmail.com>
2020-05-28 00:26:41 +0300

    Good Luck!

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,24 @@
-$(".menu-6").on('click', function () {
-    $(this).find(".hamburgerIcon").toggleClass("open");
+var inputGroups = [...document.querySelectorAll(".input-group")];
+
+inputGroups.forEach(inputGroup => {
+  var label = inputGroup.children[0];
+  var input = inputGroup.children[1];
+  input.addEventListener("focus", function(){
+    onSelect(label);
+  });
+  input.addEventListener("blur", function(){
+    onBlur(label, input);
+  })
 });
+
+function onSelect(label) {
+  label.classList.add('selected');
+}
+
+function onBlur(label, input) {
+  if (!input.value) {
+    label.classList.remove('selected'); 
+  }
+}
+
+console.log("1123");
